097d2f5a19ffede16cb4b28cc73584b7ca0c477c
AlenaBorodacheva <alien_borodach@mail.ru>
2021-01-17 20:13:38 +0300

    Первое домашнее задание сделано

---
+++ 1.Algorithms/1.Algorithms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SimpleNumer(10));
+            Console.WriteLine(SimpleNumer(5));
+            Console.ReadLine();
+        }
+        static string SimpleNumer(int n)
+        {
+            int d = 0;
+            int i = 2;
+            while (i < n)
+            {
+                if (n % i == 0)
+                    d++;
+                i++;
+            }
+            if (d == 0)
+                return "Простое";
+            else
+                return "Не простое";
+        }
+    }
+}
---
+++ 1.Algorithms/3.Fibonacci/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите положительный индекс члена числа Фибоначчи: \t");
+            int number = int.Parse(Console.ReadLine());
+
+            //Закомментируйте одну из двух следующих строчек:
+
+            //Console.WriteLine(WithRecursion(number));
+            Console.WriteLine(WithoutRecursion(number));
+
+            Console.ReadLine();
+        }
+        static string WithRecursion(int number)
+        {
+            int num1 = 0;
+            int num2 = 1;
+            if (number == 1)
+                return $"{num1}";
+            else if (number == 2)
+                return $"{num2}";
+            else
+                return WithRecursion(number - 1) + WithRecursion(number - 2);
+        }
+        static string WithoutRecursion(int number)
+        {
+            int num1 = 0;
+            int num2 = 1;
+            int n = 0;
+            if (number == 1)
+                return $"{num1}";
+            else if (number == 2)
+                return $"{num2}";
+            else
+            {
+                for (int i = 2; i < number; i++)
+                {
+                    n = num1 + num2;
+                    num1 = num2;
+                    num2 = n;
+                }
+                return $"{n}";
+            }
+        }
+    }
+}
