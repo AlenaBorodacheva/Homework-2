c7d4fd3a629b00d1aa7a6076a245fce8decd09d8
AlenaBorodacheva <alien_borodach@mail.ru>
2021-02-12 12:26:24 +0300

    восьмое домашнее задание сделано

---
+++ Sort/Sort/BucketAndExternalSort.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    class BucketAndExternalSort
+    {
+        public int[] BucketSort(int[] array)
+        {
+            var a = new List<int>();
+            var b = new List<int>();
+            var c = new List<int>();
+            var d = new List<int>();
+            var e = new List<int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 20)                
+                    a.Add(array[i]);                
+                else if (array[i] >= 20 && array[i] < 40)                
+                    b.Add(array[i]);                
+                else if (array[i] >= 40 && array[i] < 60)                
+                    c.Add(array[i]);                
+                else if (array[i] >= 60 && array[i] < 80)                
+                    d.Add(array[i]);                
+                else                
+                    e.Add(array[i]);                
+            }
+
+            if (a.Count != 0)
+                QuickSort(a, 0, a.Count - 1);
+            if (b.Count != 0)
+                QuickSort(b, 0, b.Count - 1);
+            if (c.Count != 0)
+                QuickSort(c, 0, c.Count - 1);
+            if (d.Count != 0)
+                QuickSort(d, 0, d.Count - 1);
+            if (e.Count != 0)
+                QuickSort(e, 0, e.Count - 1);
+           
+            var list1 = Merge(a, b);
+            var list2 = Merge(c, d);
+            var list3 = Merge(list1, list2);
+            var list4 = Merge(list3, e);
+
+            return ArrayFromList(list4);
+
+        }
+        public void QuickSort(List<int> list, int first, int last)
+        {
+            int i = first, j = last, x = list[(first + last) / 2];
+            do
+            {
+                while (list[i] < x)
+                    i++;
+                while (list[j] > x)
+                    j--;
+
+                if (i <= j)
+                {
+                    if (list[i] > list[j])
+                    {
+                        var tmp = list[i];
+                        list[i] = list[j];
+                        list[j] = tmp;
+                    }
+                    i++;
+                    j--;
+                }
+            } 
+            while (i <= j);
+
+            if (i < last)
+                QuickSort(list, i, last);
+            if (first < j)
+                QuickSort(list, first, j);
+        }
+        
+        public void QuickSort(int[] array, int first, int last)
+        {
+            int i = first, j = last, x = array[(first + last) / 2];
+            do
+            {
+                while (array[i] < x)
+                    i++;
+                while (array[j] > x)
+                    j--;
+
+                if (i <= j)
+                {
+                    if (array[i] > array[j])
+                    {
+                        var tmp = array[i];
+                        array[i] = array[j];
+                        array[j] = tmp;
+                    }
+
+                    i++;
+                    j--;
+                }
+            } 
+            while (i <= j);
+
+            if (i < last)
+                QuickSort(array, i, last);
+            if (first < j)
+                QuickSort(array, first, j);
+        }
+
+        public List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> list = new List<int>();
+            int leftItem = 0;
+            int rightItem = 0;
+            while (leftItem < left.Count)
+            {
+                while (rightItem < right.Count)
+                {
+                    if (left[leftItem] < right[rightItem])
+                    {
+                        list.Add(left[leftItem]);
+                        leftItem++;
+                        break;
+                    }
+                    else
+                    {
+                        list.Add(right[rightItem]);
+                        rightItem++;
+                    }
+                }
+                if (rightItem == right.Count || leftItem == left.Count)
+                    break;
+            }
+
+            if (leftItem != left.Count)
+            {
+                for (int k = leftItem; k < left.Count; k++)
+                {
+                    list.Add(left[k]);
+                }
+            }
+
+            if (rightItem != right.Count)
+            {
+                for (int k = rightItem; k < right.Count; k++)
+                {
+                    list.Add(right[k]);
+                }
+            }
+
+            return list;
+        }
+
+        public int[] Merge(int[] left, int[] right)
+        {
+            int[] arr = new int[left.Length + right.Length];
+            int leftItem = 0;
+            int rightItem = 0;
+            while (leftItem < left.Length)
+            {
+                while (rightItem < right.Length)
+                {
+                    if (left[leftItem] < right[rightItem])
+                    {
+                        arr[leftItem + rightItem] = left[leftItem];
+                        leftItem++;
+                        break;
+                    }
+                    else
+                    {
+                        arr[leftItem + rightItem] = right[rightItem];
+                        rightItem++;
+                    }
+                }
+                if (rightItem == right.Length || leftItem == left.Length)
+                    break;
+            }
+
+            if (leftItem != left.Length)
+            {
+                for (int k = leftItem; k < left.Length; k++)
+                {
+                    arr[right.Length + k] = left[k];
+                }
+            }
+
+            if (rightItem != right.Length)
+            {
+                for (int k = rightItem; k < right.Length; k++)
+                {
+                    arr[left.Length + k] = right[k];
+                }
+            }
+
+            return arr;
+        }
+
+        public int[] ArrayFromList(List<int> list)
+        {
+            int[] arr = new int[list.Count];
+            for(int i = 0; i < list.Count; i++)
+            {
+                arr[i] = list[i];
+            }
+            return arr;
+        }
+    }
+}
---
+++ Sort/Sort/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BucketAndExternalSort sort = new BucketAndExternalSort();
+            Random rnd = new Random();
+
+            var arr = CreateArr(12, rnd);
+            Console.WriteLine("Изначальный массив:");
+            Print(arr);
+
+            var BucketSortArr = sort.BucketSort(arr);
+            Console.WriteLine("Отсортированный с помощью Bucket Sort:");
+            Print(BucketSortArr);
+
+            //Не до конца поняла, чем в домашке должны отличаться BucketSort от ExternalSort. 
+            //У ExternalSort алгоритм такой:
+
+            var block1 = CreateArr(12, rnd);
+            var block2 = CreateArr(10, rnd);
+            var block3 = CreateArr(8, rnd);
+
+            Console.WriteLine("Три массива, взятых со стороннего файла:");
+            Print(block1);
+            Print(block2);
+            Print(block3);
+
+            sort.QuickSort(block1, 0, block1.Length - 1);
+            sort.QuickSort(block2, 0, block2.Length - 1);
+            sort.QuickSort(block3, 0, block3.Length - 1);
+
+            var arr1 = sort.Merge(block1, block2);
+            var result = sort.Merge(arr1, block3);
+            Console.WriteLine("Отсортированный с помощью External Sort:");
+            Print(result);
+
+            Console.ReadLine();
+        }
+        static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine("\n");
+        }
+
+        static int[] CreateArr(int size, Random rnd)
+        {
+            
+            int[] arr = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = rnd.Next(100);
+            }
+            return arr;
+        }
+    }
+}
